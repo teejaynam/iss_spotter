cab1b4ac9a0978f072ab1f8d627360d698f4cffa
TJ <teejaynam@gmail.com>
2023-06-18 22:28:43 -0400

    update index.js to require fetchCoordsByIP function and call it

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+let ipaddr;
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +9,16 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+  //ipaddr = ip Why cant it take ipaddr when I assign it 'ip'??
+});
+
+ipaddr = '99.240.253.215';
+
+fetchCoordsByIP(ipaddr, (error, data) => {
+  if (error) {
+    console.log("Failed to fetch!",error);
+    return;
+  }
+  
+  console.log("Fetched coords:", data);
 });
