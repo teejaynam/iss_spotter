9436bd70fb079c2c0f88a686e10300e4707c77df
TJ <teejaynam@gmail.com>
2023-06-18 23:00:41 -0400

    add printPassTimes function that takes in number of passTimes we get from fetchISSFlyOverTimes and prints them in a format that converts the risetimes to dates and parses the duration times

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
+};
 
-  console.log('It worked! Returned IP:' , ip);
-
-  fetchCoordsByIP(ip, (error, data) => {
-    if (error) {
-      console.log("Failed to fetch!",error);
-      return;
-    }
-    
-    console.log("Fetched coords:", data);
-
-    fetchISSFlyOverTimes(data, (error, passTimes) => {
-      if (error) {
-        console.log("It didnt work",error);
-        return;
-      }
-
-      console.log("It works. Returned flyover times:", passTimes);
-    });
-  });
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
